fbfb483d5f29340dd89da1285177f349ea27e0de
Deep <thetestcoder@gmail.com>
2023-07-26 05:31:53 +0530

    basic class structure

---
+++ 2. object-oriented/basic-class.py
@@ -0,0 +1,13 @@
+"""
+Simple class illustration:
+
+this is how you can make a class and its object
+"""
+
+class Person:
+    name = "The Test Coder"
+
+
+personObject = Person
+
+print(personObject.name)
