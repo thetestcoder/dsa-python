b86387e241362400506d612a5b6dea67eac60943
Deep <thetestcoder@gmail.com>
2023-08-04 04:41:36 +0530

    --WIP--

---
+++ 5. stacks/postfix_expression.py
@@ -0,0 +1,27 @@
+from queue import LifoQueue
+
+def compute(num1, num2, op):
+    if (op == '+'):
+        return num1+num2
+    elif (op == '-'):
+        return num1-num2
+    elif (op == '*'):
+        return num1*num2
+    elif (op == '/'):
+        return num1/num2
+    
+def evaluate_postfix(input_string):
+    stack = LifoQueue(maxsize=len(input_string))
+    str_split = input_string.split(' ')
+    print(str_split)
+    for i in range(len(str_split)):
+        if(str_split[i].isnumeric() == True):
+            stack.put(int(str_split[i]))
+        else:
+            num1 = stack.get()
+            num2 = stack.get()
+            val = compute(num1, num2, str_split[i])
+            stack.put(val)
+    return stack.get()
+
+print(evaluate_postfix('2 3 4 5 + *'))
