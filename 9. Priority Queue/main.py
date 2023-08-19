9348a815fa824690e54285fa7e1ee142a7fcc040
Deep <thetestcoder@gmail.com>
2023-08-19 08:52:05 +0530

    simple program

--- 9. Priority Queue/main.py
+++ 9. Priority Queue/main.py
@@ -1,14 +1,20 @@
-import heapq
+from heapq import *
 
-myheap = [12, 34, 24, 55, 79, 19, 47]
+def greater_equal(li, value, index):
+    if(li[index] > value):
+        return 0
+    else:
+        left = 0
+        right = 0
+        if (2 * index + 1 < len(li)):
+            left = greater_equal(li, value, 2* index+1)
+        if( 2 * index + 2 < len(li)):
+            right = greater_equal(li, value, 2 * index + 2)
+        return 1 + left + right
 
-heapq.heapify(myheap)
-print(myheap)
+li = [5, 7, 9, 1, 3] 
+heapify(li)
+value = 10
+index = 0
 
-print("Heap Push")
-heapq.heappush(myheap, 8)
-print(myheap)
-
-print("Heap pop")
-heapq.heappop(myheap)
-print(myheap)
+print(greater_equal(li, value, index))
