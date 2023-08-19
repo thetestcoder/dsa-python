df0b68cdca5916cd04a8e15c3f6058ce9b37f503
Deep <thetestcoder@gmail.com>
2023-08-19 07:11:51 +0530

    SortedPriorityQueue class added

---
+++ 9. Priority Queue/SortedPriorityQueue.py
@@ -0,0 +1,34 @@
+from PriorityQueueBase import PriorityQueueBase
+from EmptyException import EmptyException
+from PositionalList import PositionalList
+
+class SortedPriorityQueue(PriorityQueueBase):
+    
+    def __init__(self):
+        self._data = PositionalList()
+
+    def __len__(self):
+        return len(self._data)
+    
+    def add(self, key, value):
+        newest =  self._Item(key, value)
+        walk = self._data.last()
+        if walk is None:
+            self._data.add_first(walk)
+        else:
+            self._data.add_after(walk, newest)
+    
+
+    
+    def min(self):
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+    
+    def remove_min(self):
+        if self.is_empty(): raise EmptyException("Priority Queue is Empty")
+        p = self._find_min()
+        item = self._data.delete(self._data.first())
+        return (item._key, item._value)
+    
+    
