00ff5ccb89397c221a49deecb735bfb6f3775527
Deep <thetestcoder@gmail.com>
2023-09-01 19:57:25 +0530

    sort dict

---
+++ 12. sorting/sorting_dict.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def count_sort_dict(a):
+    b, c  = [], defaultdict(list)
+
+    for x in a:
+        c[x].append(x)
+    for k in range(min(c), max(c) + 1):
+        b.extend(c[k])
+    return b
+    
+seq = [6, 3, 2, 6, 4, 1, 0, 3, 5, 6, 2, 5, 4, 1, 5, 3]
+print(count_sort_dict(seq))
